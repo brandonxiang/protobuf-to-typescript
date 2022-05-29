6c756fce356e661300c27ff1a9a8cdec55b2c003
liqi.shi <liqi.shi@shopee.com>
2022-05-29 19:01:45 +0800

    feat: add vscode package

---
+++ packages/vscode/README.md
@@ -0,0 +1,25 @@
+# vscode-protobuf-to-typescript
+
+## Usage
+
+We provide two ways to convert protobuf to typescript
+
+### convert from selection
+
+- STEP 1: Select the protobuf of the current active window
+
+- STEP 2: Open the vscode command palette (How to open please refer to [Vscode Official Website](https://code.visualstudio.com/docs/getstarted/userinterface#_command-palette))
+
+- STEP 3: Search **"pts:from selection"**, execute
+
+### convert from clipboard
+
+- STEP 1: Copy protobuf from somewhere
+
+- STEP 2: Open the vscode command palette (How to open please refer to [Vscode Official Website](https://code.visualstudio.com/docs/getstarted/userinterface#_command-palette))
+
+- STEP 3: Search **"pts:from clipboard"**, execute，the converted typescript will be filled at the cursor of the currently active window
+
+## Problem
+
+If you have any questions, you can contact me by email **@kukudeshiyi1999@gmail.com**
---
+++ packages/vscode/src/lib.ts
@@ -0,0 +1,36 @@
+import { window } from "vscode";
+import * as copyPaste from "copy-paste";
+
+
+export function getClipboardText() {
+  try {
+    return Promise.resolve(copyPaste.paste());
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export function handleError(message:string,error: Error) {
+  window.showErrorMessage(`[pb-to-ts error]:${message}\n${error.message}`);
+}
+
+export function pasteToMarker(content: string) {
+  const { activeTextEditor } = window;
+
+  return activeTextEditor?.edit(editBuilder => {
+    editBuilder.replace(activeTextEditor.selection, content);
+  });
+}
+
+export function getSelectedText(): Promise<string> {
+  const { selection, document } = window.activeTextEditor!;
+  return Promise.resolve(document.getText(selection).trim());
+}
+
+export const validateLength = (text: any) => {
+  if (text.length === 0) {
+    return Promise.reject(new Error("Nothing selected"));
+  } else {
+    return Promise.resolve(text);
+  }
+};
