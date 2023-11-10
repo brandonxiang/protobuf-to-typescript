ec253ba27c1aa7852f2f243d9696eca034be86fd
brandon.xiang <1542453460@qq.com>
2023-11-10 18:53:48 +0800

    chore: open a new page

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,4 +1,4 @@
-import { ExtensionContext, commands } from "vscode";
+import { ExtensionContext, ViewColumn, commands, window } from "vscode";
 import {
   handleError,
   getClipboardText,
@@ -16,6 +16,9 @@ export function activate(context: ExtensionContext) {
   );
   context.subscriptions.push(
     commands.registerCommand("pbToTypescript.fromClipboard", transformFromClipboard)
+  );
+  context.subscriptions.push(
+    commands.registerCommand("pbToTypescript.convertOnAir", transformOnAir)
   );
 }
 
@@ -46,4 +49,29 @@ function transformFromClipboard() {
 		.catch(e=>{
 			handleError(`The Pb in clipboard may be wrong`,e);
 		});
+}
+
+function transformOnAir () {
+  const panel = window.createWebviewPanel(
+    'catCoding', // Identifies the type of the webview. Used internally
+    'Cat Coding', // Title of the panel displayed to the user
+    ViewColumn.One, // Editor column to show the new webview panel in.
+    {} // Webview options. More on these later.
+  );
+
+  panel.webview.html = getWebviewContent();
+}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
 }
